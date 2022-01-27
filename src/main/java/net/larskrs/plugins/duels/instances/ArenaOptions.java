37b8d66b8ea25770e3887e6bf04c63a7a65ee149
Larskrs <56251963+Larskrs@users.noreply.github.com>
2022-01-27 13:13:31 +0100

    Added ArenaOptions init

--- src/main/java/net/larskrs/plugins/duels/instances/ArenaOptions.java
+++ src/main/java/net/larskrs/plugins/duels/instances/ArenaOptions.java
@@ -1,7 +1,15 @@
 package net.larskrs.plugins.duels.instances;
 
 public class ArenaOptions {
-    public String GameType;
+    Public Arena arena;
+    public GameType type;
     public int winAmount;
     public boolean preMatchMeeting;
+    
+    public ArenaOptions(Arena arena, GameType type, int winAmount, boolean preMatchMeeting) {
+        this.arena = arena;
+        this.type = type;
+        this.winAmount = winAmount;
+        this.preMatchMeeting = preMatchMeeting;
+    }        
 }
