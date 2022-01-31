4f5f97c118014b4a5acc2ab5c35f32ebadf8125c
Larskrs <56251963+Larskrs@users.noreply.github.com>
2022-01-31 08:32:40 +0100

    Create LiveGameTimer.java

---
+++ src/main/java/net/larskrs/plugins/duels/instances/LiveGameTimer.java
@@ -0,0 +1,40 @@
+package net.larskrs.plugins.duels.instances;
+
+import net.larskrs.plugins.duels.Duels;
+import net.larskrs.plugins.duels.enums.GameState;
+import net.larskrs.plugins.duels.managers.ConfigManager;
+import org.bukkit.ChatColor;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class LiveGameTimer extends BukkitRunnable {
+
+    private Duels duels;
+    private Arena arena;
+    private int gameTime;
+
+    public LiveGameTimer (Duels duels, Arena arena) {
+        this.duels = duels;
+        this.arena = arena;
+        this.startupTime = ConfigManager.getStartupTime();
+        
+    }
+
+    public void start () {
+        
+
+        runTaskTimer(duels, 0, 20);
+
+    }
+
+    @Override
+    public void run() {
+        if (gameTime == 10) {
+            
+            arena.getGame().endGame();
+            return; // Stop the countdown from going any lower.
+        } else if (gameTime <= 0) {
+          arena.reset(true);
+        }   
+        startupTime--;
+    }
+}
